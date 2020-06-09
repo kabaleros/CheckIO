4f50bfe0cdf6745651375c386fc3079c26176b73
kabaleros <d.loboda@outlook.com>
2020-06-09 17:07:22 +0300

    First Word

---
+++ Acceptable Password 1.py
@@ -0,0 +1,15 @@
+# You are the beginning of a password series. Every mission will be based on the previous one. Going forward the missions will become slightly more complex.
+#
+# In this mission you need to create a password verification function.
+#
+# Those are the verification conditions:
+#
+# the length should be bigger than 6.
+# Input: A string.
+#
+# Output: A bool.
+#
+# Example:
+#
+# is_acceptable_password('short') == False
+# is_acceptable_password('muchlonger') == True
